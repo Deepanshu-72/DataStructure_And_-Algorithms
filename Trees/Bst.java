a128a164f160ae4882ae69b7e150c331e97d763f
Deepanshu Bariya <deepanshubariya72@gmail.com>
2025-11-09 22:59:34 +0530

    added BST

---
+++ Trees/Bst.java
@@ -0,0 +1,98 @@
+public class Bst {
+
+    class Node{
+        private int value;
+        private Node left;
+        private Node right;
+        private int height;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+        public int getValue(){
+            return  value;
+        }
+
+
+        private Node root;
+
+        public Bst(){
+
+        }
+
+        public  int height(Node node){
+            if(node == null){
+                return -1;
+            }
+
+            return node.height;
+        }
+
+        public boolean isEmpty(){
+            return (root == null);
+        }
+
+        public void insert(int value){
+            return insert(root, value);
+        }
+
+        private Node insert(Node node, int value){
+            if(node == null){
+                node = new Node(value);
+                return node;
+            }
+
+            if(value> node.value){
+                node.right = insert(node.right, value);
+
+            }
+
+            if(value< node.value){
+                node.left = insert(node.left, value);
+
+            }
+
+            node.height= Math.max(height(node.left), height(node.right)) + 1;
+
+            return node;
+        }
+
+        public void populate(int[] nums) {
+            for (int i = 0; i < nums.length; i++) {
+                this.insert(nums[i]);
+            }
+        }
+
+        public void populatedSorted(int[] nums) {
+            populatedSorted(nums, 0, nums.length);
+        }
+
+        private void populatedSorted(int[] nums, int start, int  end){
+            if (start >= end) {
+                return;
+            }
+
+            int mid = (start + end) / 2;
+
+            this.insert(nums[mid]);
+            populatedSorted(nums, start, mid);
+            populatedSorted(nums, mid + 1, end);
+        }
+
+        public boolean balanced() {
+            return balanced(root);
+        }
+
+        private boolean balanced(Node node) {
+            if (node == null) {
+                return true;
+            }
+            return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
+        }
+
+
+
+
+    }
+}
