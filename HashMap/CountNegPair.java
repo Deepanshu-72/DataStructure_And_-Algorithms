6a638a8dac63110a712be747e53901b0302f14ff
Deepanshu Bariya <deepanshubariya72@gmail.com>
2025-12-16 10:32:53 +0530

    count pair 2

--- HashMap/CountNegPair.java
+++ HashMap/CountNegPair.java
@@ -1,2 +1,29 @@
+import java.util.HashMap;
+
 public class CountNegPair {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 7, 1};
+        int k = 6;
+        System.out.println(count(arr, k));
+
+    }
+
+    static int count(int arr[], int k){
+        int count =0 ;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i= arr.length-1 ; i>= 0; i--){
+            int need = arr[i] - k;
+
+            if(map.containsKey(need)){
+                count += map.get(need);
+            }
+
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1 );
+        }
+
+        return count;
+
+    }
 }
